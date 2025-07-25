e9a8cada214e0dda3afc0d24bbda78433d660420
ARCHANAARUMUGAM02 <archanaarumugam0017@gmail.com>
2025-07-25 14:23:05 +0530

    Add files via upload

---
+++ verilog assignments/assignment_2/reg_comparison.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.07.2025 14:13:41
+// Design Name: 
+// Module Name: reg_comparison
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reg_comparison(
+
+    );
+endmodule
