a50fe597104ffcbccd71979ac9a4750a106ad0d7
ARCHANAARUMUGAM02 <archanaarumugam0017@gmail.com>
2025-07-25 17:45:08 +0530

    Add files via upload

---
+++ verilog assignments/assignment_2/reall_ttime.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.07.2025 17:37:53
+// Design Name: 
+// Module Name: reall_ttime
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reall_ttime();
+real delta_1,delta_2;
+initial begin
+delta_1=4e10;
+delta_2=2.18;
+end
+endmodule
