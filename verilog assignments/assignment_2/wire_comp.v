1dd9c785617bef8bc8ec0338fae34855bc301043
ARCHANAARUMUGAM02 <archanaarumugam0017@gmail.com>
2025-07-25 13:52:55 +0530

    Add files via upload

---
+++ verilog assignments/assignment_2/wire_comp.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.07.2025 11:13:27
+// Design Name: 
+// Module Name: wire_comp
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wire_comp;
+wire a,b;
+assign a=1'b0;
+assign b=a;
+endmodule
---
+++ verilog assignments/assignment_2/wire_comp_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.07.2025 11:19:41
+// Design Name: 
+// Module Name: wire_comp_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wire_comp_tb();
+reg a,b;
+wire_comp uut();
+initial begin
+a= 1'b0;
+b=a;
+end
+endmodule
