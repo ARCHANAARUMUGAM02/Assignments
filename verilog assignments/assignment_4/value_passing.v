922e06a99d23f5efe2aaeb4e778b58444f22c27d
ARCHANAARUMUGAM02 <archanaarumugam0017@gmail.com>
2025-07-30 20:57:31 +0530

    Add files via upload

---
+++ verilog assignments/assignment_4/value_passing.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.07.2025 20:39:47
+// Design Name: 
+// Module Name: value_passing
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module value_passing(output [31:0]b);
+  real a = 14.5;
+   assign b=a; 
+endmodule
+
+
