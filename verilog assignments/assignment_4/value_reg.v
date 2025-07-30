2a5b4914995bb0b9904df69619953183346d1137
ARCHANAARUMUGAM02 <archanaarumugam0017@gmail.com>
2025-07-30 19:18:48 +0530

    Add files via upload

---
+++ verilog assignments/assignment_4/value_reg.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.07.2025 18:59:28
+// Design Name: 
+// Module Name: value_reg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module value_reg( );
+reg [5:0] y;
+initial
+begin
+ y= 0*2;
+end
+endmodule
