ee9e6e0e6972ba744075b88bf07a1971b65607fb
ARCHANAARUMUGAM02 <archanaarumugam0017@gmail.com>
2025-07-31 11:28:20 +0530

    Add files via upload

---
+++ verilog assignments/assignment_4/qn_6.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.07.2025 21:24:29
+// Design Name: 
+// Module Name: qn_6
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test();
+parameter WIDTH=8;
+endmodule
+module top;
+test #(16) OUT ( );
+endmodule
